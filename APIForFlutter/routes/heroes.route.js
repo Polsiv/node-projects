71adc9c546183941e33062a9a5acff591ead5369
Polsiv <forenks1005@gmail.com>
2025-09-28 14:58:35 -0500

    api-for-flutter

---
+++ APIForFlutter/routes/heroes.route.js
@@ -0,0 +1,37 @@
+const { Router } = require('express');
+
+const { heroesGet,
+        heroeIdGet,
+        heroesComoGet,
+        heroesPost,
+        heroePut,
+        heroeDelete,
+} = require('../controllers/heroes.controller');
+
+const { validarJWT} = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+//3 Retrieve
+router.get('/', 
+    validarJWT,
+    heroesGet);
+
+router.get('/:id', heroeIdGet);
+
+router.get('/como/:termino', 
+    validarJWT,
+    heroesComoGet);
+
+
+//CREATE
+router.post('/', heroesPost);
+
+//UPDATE
+router.put('/:id', heroePut);
+
+//DELETE
+router.delete('/:id', heroeDelete);
+
+
+module.exports = router;
